ea1d7268a38e715eb5eb41a970e22e3aaeaa5dba
rajatelagathoti-19 <24kq1a61c6@pace.ac.in>
2026-03-11 04:35:15 +0000

    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Sample Piece")
+st.write("Sample Piece ey ra babu inkem choostav..kinda choodu")
+st.write("Gaddi tinamanta tintaava 🙄..pora")
